fbeb661c82f61c233b19f3fc317d514d278a8958
Arush Mittal <arush.m.mittal@gmail.com>
2025-02-11 21:52:04 +0530

    Solution to Print a Pyramid Pattern

---
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int size, i, j;
+    char character;
+
+    printf("Enter the size of the square: ");
+    scanf("%d", &size);
+
+    if (size <= 0) {
+        printf("Please enter a positive integer for the size.\n");
+        return 1;
+    }
+
+    printf("Enter the character to print: ");
+    scanf(" %c", &character);
+
+    for (i = 1; i <= size; i++) {
+        for (j = 1; j <= size; j++) {
+            if (i == 1 || i == size || j == 1 || j == size) {
+            
+                printf(" %c ", character);
+            } else {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
